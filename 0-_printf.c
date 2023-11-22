b9fce6a3301ec9cc79327a292110823e68ce0ef3
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-22 13:20:48 -0400

    added character, string and percent print functions

---
+++ 0-_printf.c
@@ -0,0 +1,6 @@
+#include <stdarg.h>
+#include <uninstd.h>
+
+int _printf(const char *format, ...)
+
+
--- main.h
+++ main.h
@@ -1,3 +1,4 @@
-int _printf(const char *format, ...);
-int _strlen(char *s);
+void print_character(char letter);
+void print_string(char *str);
+void print_percent(char percent);
 
