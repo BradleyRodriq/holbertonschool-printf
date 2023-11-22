3ff5237afc43733f98f4518cfd8525e59d18672d
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-22 13:52:11 -0400

    added 0-_printf.c that takes format specifiers and prints them

--- 0-_printf.c
+++ 0-_printf.c
@@ -1,6 +1,46 @@
 #include <stdarg.h>
-#include <uninstd.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *
+ */
 
 int _printf(const char *format, ...)
 
+{
+	va_list arguments;
+	va_start(arguments, format);
 
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				print_character(va_arg(arguments,int));
+			}
+			else if (*format == 's')
+			{
+				print_string(va_arg(arguments, char *));
+			}
+			else if (*format == '%')
+			{
+				print_percent('%');
+			}
+			else
+			{
+				write(STDOUT_FILENO, "?", 1);
+				write(STDOUT_FILENO, format, 1);
+			}
+		}
+		else
+		{
+			write(STDOUT_FILENO, format, 1);
+		}
+		format++;
+	}
+	va_end(arguments);
+	return (0);
+}
--- main.h
+++ main.h
@@ -1,4 +1,5 @@
 void print_character(char letter);
 void print_string(char *str);
 void print_percent(char percent);
+int _printf(const char *format, ...);
 
