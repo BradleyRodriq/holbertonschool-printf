4c1c7c2ec8ebf10cc7cda9304f7ffc4a1bc62f4c
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-24 13:32:15 -0400

    added count to be able to return number of printed characters

--- function_helpers.c
+++ function_helpers.c
@@ -12,6 +12,22 @@ void print_character(char letter)
 }
 
 /**
+ *_strlen - prints the length of a string
+ *@s: string to be counted
+ *Return: length of string
+ */
+int _strlen(char *s)
+
+
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  *
  */
 void print_string(char *str)
--- main.h
+++ main.h
@@ -2,3 +2,4 @@ void print_character(char letter);
 void print_string(char *str);
 void print_percent(char percent);
 int _printf(const char *format, ...);
+int _strlen(char *s);
