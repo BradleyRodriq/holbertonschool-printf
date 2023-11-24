a8f6edb67e5d3e29da090d1a0e5309069d96195b
leomar <leomar.rodriguez1@upr.edu>
2023-11-24 14:56:10 -0400

    testing push to test va_arg checkr

--- _printf.c
+++ _printf.c
@@ -4,50 +4,53 @@
 
 /**
  *
- */
+ /
 
-int _printf(const char *format, ...)
+int _printf(const charformat, ...)
 
 {
-	int count = 0;
+    int count = 0;
 
-	va_list arguments;
-	va_start(arguments, format);
+    va_list arguments;
+    va_start(arguments, format);
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				print_character(va_arg(arguments,int));
-				count ++;
-			}
-			else if (*format == 's')
-			{
-				print_string(va_arg(arguments, char *));
-				count += _strlen(va_arg(arguments, char *));
-			}
-			else if (*format == '%')
-			{
-				print_percent('%');
-				count++;
-			}
-			else
-			{
-				write(STDOUT_FILENO, "%", 1);
-				write(STDOUT_FILENO, format, 1);
-				count += 2;
-			}
-		}
-		else
-		{
-			write(STDOUT_FILENO, format, 1);
-			count++;
-		}
-		format++;
-	}
-	va_end(arguments);
-	return (count);
+    while (format != '\0')
+    {
+        if (format == '%')
+        {
+            format++;
+            if (format == 'c')
+            {
+                print_character(va_arg(arguments,int));
+                count ++;
+            }
+            else if (format == 's')
+            {
+                char str;
+
+                str = va_arg(arguments, char);
+                print_string(str);
+                count += _strlen(str);
+            }
+            else if (*format == '%')
+            {
+                print_percent('%');
+                count++;
+            }
+            else
+            {
+                write(STDOUT_FILENO, "%", 1);
+                write(STDOUT_FILENO, format, 1);
+                count += 2;
+            }
+        }
+        else
+        {
+            write(STDOUT_FILENO, format, 1);
+            count++;
+        }
+        format++;
+    }
+    va_end(arguments);
+    return (count);
 }
