cb57b98579b8e8424b015f5fc2189869eaf2685a
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-25 00:54:31 -0400

    added some betty compliance to _printf

--- _printf.c
+++ _printf.c
@@ -27,24 +27,34 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				return (-1);
 			else if (*format == 'c')
+			{
 				print_character(va_arg(arguments, int));
 				count++;
+			}
 			else if (*format == 's')
+			{
 				char *str = va_arg(arguments, char *);
 
 				if (str == NULL)
 					str = "(null)";
 				print_string(str);
 				count += _strlen(str);
+			}
 			else if (*format == '%')
+			{
 				print_percent('%');
 				count++;
+			}
 			else
+			{
 				count += default_case(format);
+			}
 		}
 		else
+		{
 			write(STDOUT_FILENO, format, 1);
 			count++;
+		}
 		format++;
 	}
 	va_end(arguments);
