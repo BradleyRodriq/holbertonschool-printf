1ceca2ddf985724936b52a08b9f4b10b4da5af21
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2023-11-26 01:20:26 -0400

    fixed count for non specifier characters after %

--- function_helpers.c
+++ function_helpers.c
@@ -1,50 +1,10 @@
 #include <unistd.h>
-<<<<<<< HEAD
-=======
 #include <stdio.h>
->>>>>>> 9a490ca5157c0bd3834e22e260366c2c31fdf038
 #include <stdarg.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * print_character - prints a character
- * @letter: the character to print
- */
-void print_character(char letter)
-{
-	write(STDOUT_FILENO, &letter, 1);
-}
-
-/**
- *_strlen - prints the length of a string
- *@s: string to be counted
- *Return: length of string
- */
-int _strlen(char *s)
-
-
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * print_string - prints a string
- * @str: the string to be printed
- * Return: always 0
- */
-void print_string(char *str)
-{
-	int len = 0;
-
-	if (str == NULL)
-		str = "(null)";
-=======
  *
  */
 int print_character(va_list arg)
@@ -72,24 +32,11 @@ int print_string(va_list arg)
 		write(STDOUT_FILENO, "(null)", 6);
 		return (6);
 	}
->>>>>>> 9a490ca5157c0bd3834e22e260366c2c31fdf038
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 	write(STDOUT_FILENO, str, len);
-<<<<<<< HEAD
-}
-
-/**
- * print_percent - prints a percent sign
- * @percent: the percent sign
- */
-void print_percent(char percent)
-{
-	(void) percent;
-	write(STDOUT_FILENO, "%", 1);
-=======
 	return len;
 }
 /**
@@ -100,7 +47,6 @@ int print_percent(va_list arg)
 	(void) arg;
 	write(STDOUT_FILENO, "%", 1);
 	return (1);
->>>>>>> 9a490ca5157c0bd3834e22e260366c2c31fdf038
 }
 
 /**
@@ -112,10 +58,6 @@ int print_percent(va_list arg)
 int default_case(const char *format)
 {
 	write(STDOUT_FILENO, "%", 1);
-<<<<<<< HEAD
-	write(STDOUT_FILENO, &format, 1);
-=======
 	write(STDOUT_FILENO, format, 1);
->>>>>>> 9a490ca5157c0bd3834e22e260366c2c31fdf038
 	return (2);
 }
--- main.c
+++ main.c
@@ -1,35 +1,18 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
-=======
-#include <limits.h>
-#include <stdio.h>
->>>>>>> 9a490ca5157c0bd3834e22e260366c2c31fdf038
 #include "main.h"
 
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
  * Return: 0 on success, error code otherwise
  */
 int main(void)
 {
 	int len, len2;
 
-	len = _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-	len2 = printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-=======
- * Return: Always 0
- */
-int main(void)
-{
-
-	int len, len2;
-
-	len = _printf(NULL);
-	len2 = printf(NULL);
->>>>>>> 9a490ca5157c0bd3834e22e260366c2c31fdf038
+	len = _printf("%!\n");
+	len2 = printf("%!\n");
 	fflush(stdout);
 	if (len != len2)
 	{
